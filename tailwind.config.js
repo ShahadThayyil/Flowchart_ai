88dc54e11a71cbb770d7462967058589f1ae599c
Muhammed Shahad <muhammedshahad@gmail.com>
2024-11-30 16:49:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'poppins': ['Poppins', 'sans-serif'],
+      'montserrat': ['Montserrat', 'sans-serif'],
+      'yuji':['Yuji Mai','sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
